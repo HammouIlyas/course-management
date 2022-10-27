6b5a4d11b015fe9341eb7329ac4c115912c04def
ilyas hammou <ilyashammou.prof@gmail.com>
2022-10-27 19:47:21 +0100

    commit

--- BackEnd/src/main/java/com/bezkoder/springjwt/controllers/CourseController.java
+++ BackEnd/src/main/java/com/bezkoder/springjwt/controllers/CourseController.java
@@ -3,12 +3,8 @@ package com.bezkoder.springjwt.controllers;
 import com.bezkoder.springjwt.models.metier.Course;
 import com.bezkoder.springjwt.repository.CourseRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
